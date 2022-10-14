8e30a4c08dfead6290cb79ada1c99044226e1b63
Evan Pratten <ewpratten@gmail.com>
2022-10-14 12:13:52 -0400

    Make the graphics nice

--- src/module.cpp
+++ src/module.cpp
@@ -20,12 +20,13 @@ struct A10u8r_module : Module {
 
 	A10u8r_module() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(VALUE_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(INPUT_INPUT, "");
-		configOutput(OUTPUT_OUTPUT, "");
+		configParam(VALUE_PARAM, 0.f, 1.f, 1.f, "Multiplier");
+		configInput(INPUT_INPUT, "Signal");
+		configOutput(OUTPUT_OUTPUT, "Signal");
 	}
 
 	void process(const ProcessArgs& args) override {
+
 		float value = params[VALUE_PARAM].getValue();
 		float input = inputs[INPUT_INPUT].getVoltage();
 		outputs[OUTPUT_OUTPUT].setVoltage(value * input);
@@ -38,16 +39,16 @@ struct A10u8r_moduleWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/a10u8r_module.svg")));
 
+		// Set the screw positions
+		addChild(createWidget<ScrewSilver>(Vec(0, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(0, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 64.25)), module, A10u8r_module::VALUE_PARAM));
-
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 25.81)), module, A10u8r_module::INPUT_INPUT));
-
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 110.491)), module, A10u8r_module::OUTPUT_OUTPUT));
+		// Configure I/O
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(5.08, 64.25)), module, A10u8r_module::VALUE_PARAM));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.08, 31.102)), module, A10u8r_module::INPUT_INPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(5.08, 110.491)), module, A10u8r_module::OUTPUT_OUTPUT));
 	}
 };
 
